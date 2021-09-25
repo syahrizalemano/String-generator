d0ce889c27a51429e579ca6be1606c761b505af3
syahrizalemano <89107294+syahrizalemano@users.noreply.github.com>
2021-09-25 13:03:15 +0800

    Update genStr.py

--- plugins/genStr.py
+++ plugins/genStr.py
@@ -28,7 +28,7 @@ Buat string Session disini.
 Masukan `API_ID`  Disini
 ║▌│█║▌│ █║▌│█│║▌║
 """
-IP_HASH = "Disini `API_HASH` ║▌│█║▌│ █║▌│█│║▌""".\n\nTap /cancel untuk Cancel.
+IP_HASH = "Disini `API_HASH` ║▌│█║▌│ █║▌│█│║▌""".\n\nTap /cancel untuk Cancel".
 
 PHONE_NUMBER_TEXT = (
     "📞__ Masukan nomor telephon"
